507b03c798e3d1d050cdabccbf0f50ee979d8c41
Volha Linkevich <volha.linkevich@gmail.com>
2023-06-23 17:46:10 +0400

    Add logging

--- Praxis.Bff/Program.cs
+++ Praxis.Bff/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/api/hello", () => "Hello World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-app.Run();
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+
+    var app = builder.Build();
+
+    app.MapGet("/api/hello", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
