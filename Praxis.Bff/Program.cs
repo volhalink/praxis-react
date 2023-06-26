9995a1c3cbde8f0b68c19104b601952af9072732
Volha Linkevich <volha.linkevich@gmail.com>
2023-06-26 16:30:57 +0400

    Log in with Google

--- Praxis.Bff/Program.cs
+++ Praxis.Bff/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using System;
+using System.Security.Claims;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -14,10 +22,62 @@ try
         .Enrich.FromLogContext()
         .ReadFrom.Configuration(ctx.Configuration));
 
+    builder.Services
+        .AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddGoogle(googleOptions =>
+        {
+            googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+            googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+            //googleOptions.UsePkce = true;
+        });
+
+    builder.Services.AddAuthorization();
+
+    builder.Services.Configure<ForwardedHeadersOptions>(options =>
+    {
+        options.ForwardedHeaders = ForwardedHeaders.All;
+    });
 
     var app = builder.Build();
 
-    app.MapGet("/api/hello", () => "Hello World!");
+    //app.UsePathBase("/api/");
+    app.UseHttpLogging();
+    app.UseForwardedHeaders();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapGet("/api/login", (ClaimsPrincipal user) =>
+    {
+        var properties = new AuthenticationProperties
+        {
+            RedirectUri = "/api/hello"
+        };
+
+        return Results.Challenge(properties, new[] { GoogleDefaults.AuthenticationScheme });
+    });
+
+    app.MapGet("/api/logout", (ClaimsPrincipal user) =>
+    {
+        var properties = new AuthenticationProperties
+        {
+            RedirectUri = "/api/hello"
+        };
+
+        return Results.SignOut(properties, new[] { CookieAuthenticationDefaults.AuthenticationScheme });
+    });
+
+    app.MapGet("/api/hello", (ClaimsPrincipal user) => {
+        string? name = user?.Claims?.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+        string? email = user?.Claims?.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+
+        return "Hello " + (name?? "World") + "!";   
+        });
 
     app.Run();
 }
